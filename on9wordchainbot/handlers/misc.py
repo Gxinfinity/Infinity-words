e4bc633c771e5bbb5d1c144f4835d92786cc1e6b
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-21 03:37:51 +0530

    Update misc.py

--- on9wordchainbot/handlers/misc.py
+++ on9wordchainbot/handlers/misc.py
@@ -1,7 +1,9 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import types
+from aiogram.dispatcher.filters import CommandStart, CommandHelp
+from on9wordchainbot import dp, bot
+from on9wordchainbot.utils import get_user, add_user, get_group, add_group
 
-from ..utils import get_user, add_user, get_group, add_group
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 START_TEXT = """
 Hi👋 {user}!
@@ -12,38 +14,41 @@ Play exciting word chain games with your friends in Telegram groups.
 ➕ Add me to a group and type `/start` to begin the fun!
 """
 
-@Client.on_message(filters.command(["start", "help"]))
-async def start_command(client: Client, message: Message):
-    user = message.from_user
-    chat = message.chat
+PHOTO_URL = "https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg"
 
-    # Save user
-    if not await get_user(user.id):
-        await add_user(user.id, user.username or "", user.first_name or "")
+@dp.message_handler(commands=["start", "help"])
+async def start(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.username or ""
+    first_name = message.from_user.first_name or "User"
 
-    # Group handling
-    if chat.type in ["group", "supergroup"]:
-        if not await get_group(chat.id):
-            await add_group(chat.id, chat.title)
+    # Save user if not already saved
+    if not await get_user(user_id):
+        await add_user(user_id, user_name, first_name)
 
-        await message.reply_photo(
-            photo="https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg",
-            caption=f"👋 Hello {user.mention}, I'm ready to play word chain in *{chat.title}*! 🎮\nUse `/play` to begin!",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ℹ️ Help", url=f"https://t.me/{(await client.get_me()).username}?start=help")]
-            ])
-        )
+    # Group chat handling
+    if message.chat.type in ["group", "supergroup"]:
+        if not await get_group(message.chat.id):
+            await add_group(message.chat.id, message.chat.title)
 
-    # Private (DM) handling
-    else:
-        await message.reply_photo(
-            photo="https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg",
+        await bot.send_photo(
+            chat_id=message.chat.id,
+            photo=PHOTO_URL,
             caption=START_TEXT.format(
-                user=user.mention,
-                bot=(await client.get_me()).first_name
+                user=message.from_user.get_mention(),
+                bot=(await bot.get_me()).first_name
+            )
+        )
+    else:
+        await bot.send_photo(
+            chat_id=message.chat.id,
+            photo=PHOTO_URL,
+            caption=START_TEXT.format(
+                user=message.from_user.get_mention(),
+                bot=(await bot.get_me()).first_name
             ),
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("➕ Add Me To Group", url=f"https://t.me/{(await client.get_me()).username}?startgroup=true")],
+                [InlineKeyboardButton("➕ Add Me To Group", url=f"https://t.me/{(await bot.get_me()).username}?startgroup=true")],
                 [
                     InlineKeyboardButton("👥 Group", url="https://t.me/+5vPKU47S6HNiNjY1"),
                     InlineKeyboardButton("🔄 Updates", url="https://t.me/Who_Cares_qt")
