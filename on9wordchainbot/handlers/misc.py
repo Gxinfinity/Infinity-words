e0ed1d383b899ed7f64f8cb5e6124a9043613339
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-21 03:48:36 +0530

    Update misc.py

--- on9wordchainbot/handlers/misc.py
+++ on9wordchainbot/handlers/misc.py
@@ -1,14 +1,14 @@
 from aiogram import types
-from aiogram.dispatcher.filters import CommandStart, CommandHelp
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher import Dispatcher
-
-from on9wordchainbot import dp  # ya jahan se dp import hota ho
-from ..utils import get_user, add_user, get_group, add_group
+from aiogram.dispatcher.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+from on9wordchainbot import dp
+from on9wordchainbot.utils import get_user, add_user, get_group, add_group
 
 
 START_TEXT = """
-Hi👋 {user}!
+Hi {user}!
 
 I'm {bot} – your fun companion from 𝑽𝒆𝒍𝒐𝒄𝒊𝒕𝒚 𝐗 𝗪𝗼𝗿𝗱 𝗖𝗵𝗮𝗶𝗻 🎮
 
@@ -16,40 +16,39 @@ Play exciting word chain games with your friends in Telegram groups.
 ➕ Add me to a group and type `/start` to begin the fun!
 """
 
+PHOTO_URL = "https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg"
 
-@dp.message_handler(CommandStart() | CommandHelp())
-async def start(message: types.Message):
+
+@dp.message_handler(CommandStart())
+async def start_cmd(message: Message, state: FSMContext):
     user = message.from_user
-    user_id = user.id
-    user_name = user.username or ""
-    first_name = user.first_name or "User"
+    chat = message.chat
 
     # Save user if not already saved
-    if not get_user(user_id):
-        await add_user(user_id, user_name, first_name)
+    if not await get_user(user.id):
+        await add_user(user.id, user.username or "", user.first_name or "User")
 
-    # Group chat
-    if message.chat.type in ["group", "supergroup"]:
-        if not get_group(message.chat.id):
-            await add_group(message.chat.id, message.chat.title)
+    # If group chat
+    if chat.type in ("group", "supergroup"):
+        if not await get_group(chat.id):
+            await add_group(chat.id, chat.title)
 
-        await message.reply_photo(
-            photo="https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg",
-            caption=START_TEXT.format(
-                user=message.from_user.get_mention(as_html=True),
-                bot=message.bot.first_name
-            ),
-            parse_mode="HTML"
+        await message.answer_photo(
+            photo=PHOTO_URL,
+            caption=START_TEXT.format(user=user.get_mention(), bot=(await message.bot.me).first_name),
         )
+
+    # If private chat
     else:
-        # Private chat
-        await message.reply_photo(
-            photo="https://graph.org/file/046efb7c1411d26be3145-a751e2c61b39111484.jpg",
-            caption=START_TEXT.format(
-                user=message.from_user.get_mention(as_html=True),
-                bot=message.bot.first_name
-            ),
-            parse_mode="HTML",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("➕ Add Me To Group", url=f"https://t.me/{message.bot.username}?startgroup=true")],
-                [
+        buttons = InlineKeyboardMarkup(row_width=2).add(
+            InlineKeyboardButton("➕ Add Me To Group", url=f"https://t.me/{(await message.bot.me).username}?startgroup=true"),
+            InlineKeyboardButton("👥 Group", url="https://t.me/+5vPKU47S6HNiNjY1"),
+            InlineKeyboardButton("🔄 Updates", url="https://t.me/Who_Cares_qt"),
+            InlineKeyboardButton("ɢx ᴅᴀʀᴋ ʙᴏᴛs [🇮🇳]", url="https://t.me/dark_x_knight_musiczz_support")
+        )
+
+        await message.answer_photo(
+            photo=PHOTO_URL,
+            caption=START_TEXT.format(user=user.get_mention(), bot=(await message.bot.me).first_name),
+            reply_markup=buttons
+        )
