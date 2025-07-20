bc34ecd9cffbaf9247a448fe22cc04cfd66f888b
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-21 04:24:38 +0530

    Update utils.py

--- on9wordchainbot/utils.py
+++ on9wordchainbot/utils.py
@@ -7,14 +7,11 @@ from . import bot
 from .constants import ADMIN_GROUP_ID
 from .words import Words
 
-
 def is_word(s: str) -> bool:
     return all(c in ascii_lowercase for c in s)
 
-
 def check_word_existence(word: str) -> bool:
     return word in Words.dawg
-
 
 def filter_words(
     min_len: int = 1,
@@ -34,7 +31,6 @@ def filter_words(
         words = [w for w in words if w not in exclude_words]
     return words
 
-
 def get_random_word(
     min_len: int = 1,
     prefix: Optional[str] = None,
@@ -45,7 +41,9 @@ def get_random_word(
     words = filter_words(min_len, prefix, required_letter, banned_letters, exclude_words)
     return random.choice(words) if words else None
 
+def inline_keyboard_from_button(button: types.InlineKeyboardButton) -> types.InlineKeyboardMarkup:
+    return types.InlineKeyboardMarkup(inline_keyboard=[[button]])
 
-ADD_TO_GROUP_KEYBOARD = types.InlineKeyboardMarkup(inline_keyboard=[
-    [types.InlineKeyboardButton("➕ Add Me To Group", url="https://t.me/on9wordchainbot?startgroup=true")]
-])
+ADD_TO_GROUP_KEYBOARD = inline_keyboard_from_button(
+    types.InlineKeyboardButton("➕ Add Me To Group", url="https://t.me/on9wordchainbot?startgroup=true")
+)
