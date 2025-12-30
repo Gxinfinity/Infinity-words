620c39c2df8b63fb27a9892126b43804f5d167aa
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-12-31 01:49:30 +0530

    Update __init__.py

--- on9wordchainbot/__init__.py
+++ on9wordchainbot/__init__.py
@@ -1,40 +1,59 @@
 import asyncio
-import random
-import time
-from decimal import ROUND_HALF_UP, getcontext
+import logging
+from datetime import datetime
+from typing import Dict, TYPE_CHECKING
 
-from aiogram import executor
-from periodic import Periodic
+import aiohttp
+import asyncpg
+from aiogram import Bot, Dispatcher, types
 
-from on9wordchainbot import dp, loop, pool, session
-from on9wordchainbot.utils import send_admin_group
-from on9wordchainbot.words import Words
+from .constants import DB_URI, ON9BOT_TOKEN, TOKEN
+from .filters import filters
 
-random.seed(time.time())
-getcontext().rounding = ROUND_HALF_UP
+if TYPE_CHECKING:
+    from .models import ClassicGame
+
+try:
+    import coloredlogs  # pip install coloredlogs
+except ImportError:
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+else:
+    coloredlogs.install(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+logger = logging.getLogger(name)
+
+try:
+    import uvloop  # pip install uvloop
+except ImportError:
+    logger.info(r"uvloop unavailable ¯\_(ツ)_/¯")
+else:
+    uvloop.install()
+
+loop = asyncio.get_event_loop()
+bot = Bot(TOKEN, parse_mode=types.ParseMode.MARKDOWN, disable_web_page_preview=True)
+on9bot = Bot(ON9BOT_TOKEN)
+dp = Dispatcher(bot)
+session = aiohttp.ClientSession()
+pool: asyncpg.pool.Pool
 
 
-async def on_startup(_) -> None:
-    await send_admin_group("Bot starting.")
-    await Words.update()
+class GlobalState:
+    build_time = datetime.now().replace(microsecond=0)
+    maint_mode = False
 
-    task = Periodic(3 * 60 * 60, Words.update)
-    await task.start()
-
-
-async def on_shutdown(_) -> None:
-    await asyncio.gather(session.close(), pool.close())
+    games: Dict[int, "ClassicGame"] = {}  # Group id mapped to game instance
+    games_lock: asyncio.Lock = asyncio.Lock()
 
 
-def main() -> None:
-    executor.start_polling(
-        dp,
-        loop=loop,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-    )
+async def init() -> None:
+    global pool
+    logger.info("Connecting to database")
+    pool = await asyncpg.create_pool(DB_URI, loop=loop)
 
 
-if __name__ == "__main__":
-    main()
+loop.run_until_complete(init())
+
+for f in filters:  # Need to bind filters before adding handlers
+    dp.filters_factory.bind(f)
+
+from .handlers import *
